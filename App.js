4aeb4c8ebaa2382e6a43b2929c3b84b97aca6016
jooprima <jooprima@gmail.com>
2020-01-06 21:35:22 +0700

    function component dalam 1 file

--- App.js
+++ App.js
@@ -10,6 +10,15 @@ import React, {Component} from 'react';
 import {View, Text, StatusBar} from 'react-native';
 import Header from './Header';
 
+const Footer = () => {
+  var jooprima = '@jooprima';
+  return (
+    <View>
+      <Text>Ini adalah Footer</Text>
+      <Text>{jooprima}</Text>
+    </View>
+  );
+};
 export default class App extends Component {
   render() {
     return (
@@ -18,6 +27,7 @@ export default class App extends Component {
           <StatusBar backgroundColor="grey" />
           <Header />
           <Text>First App!</Text>
+          <Footer />
         </View>
       </>
     );
