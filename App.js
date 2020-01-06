51809bbcb0e663c52dd8dff41c84756d248ea911
jooprima <jooprima@gmail.com>
2020-01-06 19:32:35 +0700

    ch04 basic component react native

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <StatusBar backgroundColor="grey" />
+        <Text>First App!</Text>
+      </View>
+    </>
+  );
+};
+
+
+export default App;
